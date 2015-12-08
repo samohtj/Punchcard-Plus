b3e2e497a3b147f4240f65f324264ad237659fa5
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2015-12-07 21:20:20 -0500

    Adds documentation

--- src/punchcard/Workday.java
+++ src/punchcard/Workday.java
@@ -2,27 +2,60 @@ package punchcard;
 
 import java.util.Date;
 
+/**
+ * The Workday class stores a continuous instance of work, with a defined start and end time.
+ * The beginning and end times can be retured as Date objects.
+ * @author Jonathan Thomas
+ *
+ */
 public class Workday {
 	private Date begin;
 	private Date end;
 	
+	/**
+	 * Create a new Workday with the given beginning and ending times.
+	 * @param begin Time the workday started.
+	 * @param end Time the workday ended.
+	 */
 	public Workday(Date begin, Date end) {
 		this.begin = begin;
 		this.end = end;
 	}
 	
+	/**
+	 * Create an empty workday.
+	 */
 	public Workday() {
 	}
 
+	/**
+	 * Get the beginning time of the workday.
+	 * @return Date when the workday started.
+	 */
 	public Date getBegin() {
 		return begin;
 	}
+	
+	/**
+	 * Set the start point for the workday.
+	 * @param begin Date when the workday started.
+	 */
 	public void setBegin(Date begin) {
 		this.begin = begin;
 	}
+	
+	/**
+	 * Get the ending time of the workday.
+	 * @return Date when the workday ended.
+	 */
 	public Date getEnd() {
 		return end;
 	}
+	
+	/**
+	 * Set the end point for the workday.
+	 * @param end Date when the workday ended.
+	 */
 	public void setEnd(Date end) {
 		this.end = end;
 	}
--- src/punchcard/gui/PuncherPanel.java
+++ src/punchcard/gui/PuncherPanel.java
@@ -13,16 +13,24 @@ import javax.swing.table.DefaultTableModel;
 
 import punchcard.Workday;
 
+/**
+ * Panel containing the punch in / punch out buttons, and a list of workdays in this payment period.
+ * @author Jonathan Thomas
+ * 
+ */
 public class PuncherPanel extends JPanel {
 	private static final long serialVersionUID = -3577936101409734832L;
 
-	JButton btnPunchIn = new JButton("Punch In");
-	JButton btnPunchOut = new JButton("Punch Out");
+	private JButton btnPunchIn = new JButton("Punch In");
+	private JButton btnPunchOut = new JButton("Punch Out");
 	
 	JTable tblWorkdays;
 	
 	Workday workdayStaging = null;
 	
+	/**
+	 * Create a new PuncherPanel.
+	 */
 	public PuncherPanel() {
 
 		add(btnPunchIn);
@@ -32,12 +40,17 @@ public class PuncherPanel extends JPanel {
 		workdayPane.add(tblWorkdays);
 		add(workdayPane);
 		
+		// Disable the punch out button. This assumes that you aren't currently punched in.
 		btnPunchOut.setEnabled(false);
 		
 		btnPunchIn.addActionListener(new PunchInListener());
 		btnPunchOut.addActionListener(new PunchOutListener());
 	}
 	
+	/**
+	 * Add a workday to the display table.
+	 * @param workday
+	 */
 	public void addWorkday(Workday workday) {
 		DefaultTableModel model = (DefaultTableModel) tblWorkdays.getModel();
 		String day = "date";
@@ -46,14 +59,26 @@ public class PuncherPanel extends JPanel {
 		model.addRow(new Object[] {day, start, end});
 	}
 	
+	/**
+	 * Custom model for the table displaying the workdays.
+	 * @author Jonathan Thomas
+	 *
+	 */
 	private class WorkdayTableModel extends DefaultTableModel {
 		private static final long serialVersionUID = 4060693810634100465L;
 
+		/**
+		 * Array of column names.
+		 */
 		private String[] columnNames = {"First Name",
                 "Last Name",
                 "Sport",
                 "# of Years",
                 "Vegetarian"};
+		
+		/**
+		 * Array of data.
+		 */
 private Object[][] data = {
 {"Kathy", "Smith",
 "Snowboarding", new Integer(5), new Boolean(false)},
@@ -67,44 +92,74 @@ private Object[][] data = {
 "Pool", new Integer(10), new Boolean(false)}
 };
 		
+		/**
+		 * Return the number of columns in the model.
+		 * @return int quantity of columns.
+		 */
 		public int getColumnCount() {
             return columnNames.length;
         }
 
+		/**
+		 * Return the number of rows displayed by the model.
+		 * @return int quantity of rows.
+		 */
         public int getRowCount() {
-        	return 1;
+        	return data.length;
         }
 
+        /**
+         * Get the name of the specified column.
+         * @return String name.
+         */
         public String getColumnName(int col) {
             return columnNames[col];
         }
 
+        /**
+         * Get the value stored at a certain position in the model.
+         * @return Object representing whatever it was.
+         */
         public Object getValueAt(int row, int col) {
             return data[row][col];
         }
 	}
 	
+	/**
+	 * PunchInListener is an action listener for the punch in button.
+	 * @author Jonathan Thomas
+	 *
+	 */
 	private class PunchInListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Punching in!");
+			// Once this button has been pressed, disable it and enable the opposite button
 			btnPunchIn.setEnabled(false);
 			btnPunchOut.setEnabled(true);
 			
+			// Create a new workday, with right now as the starting point
 			workdayStaging = new Workday();
 			workdayStaging.setBegin(new Date());
 		}
 	}
 	
+	/**
+	 * PunchOutListener is an action listener for the punch out button.
+	 * @author Jonathan Thomas
+	 *
+	 */
 	private class PunchOutListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Punchin out!");
+			// Once this button has been pressed, disable it and enable the opposite button
 			btnPunchIn.setEnabled(true);
 			btnPunchOut.setEnabled(false);
 			
+			// If the workdayStaging object isn't empty, set the ending point to right now
 			if (workdayStaging != null) {
 				workdayStaging.setEnd(new Date());
 				addWorkday(workdayStaging);
